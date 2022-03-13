27f5f24ec101cd09135b4a898b771bcd103897bb
hsh6449 <57973170+hsh6449@users.noreply.github.com>
2022-03-13 23:59:22 +0900

    3.13
    폴더 정리 및 백준 3.13 추가

---
+++ BaekJUn/BJ_03.13.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 13 23:39:36 2022
+
+@author: sanha
+"""
+
+# 1271 엄청난 부자 
+
+# m = 1000
+# n = 3
+m, n = map(int, input().split())
+
+if bool(m % n) == True:
+    least = m - (m % n)
+    deviden = least // n # / 대신에 정수값을 반환해주는 //을 사용
+    
+    print(int(deviden))
+    print(int(m - least))
+else :
+    deviden = m // n # / 대신에 정수값을 반환해주는 //을 사용
+     
+    print(int(deviden))
+    print(int(m % n))
